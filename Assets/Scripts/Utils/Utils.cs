12b15688cfabaa3a406c4ff9866e043464f8085a
davidkastl <kastl.david@gmail.com>
2015-04-28 20:45:59 +0200

    +Utils, +possibleSounds

--- Assets/Scripts/Utils/Utils.cs
+++ Assets/Scripts/Utils/Utils.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Linq;
 using System.Collections.Generic;
 
+
 public static class Utils
 {
     public static void InitBackground(Transform background, Camera mainCam)
@@ -17,32 +18,52 @@ public static class Utils
         {
             return;
         }
-
+        
         float width = spriteRenderer.sprite.bounds.size.x;
         float height = spriteRenderer.sprite.bounds.size.y;
 
         float worldScreenHeight = Camera.main.orthographicSize * 2.0f;
         float worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
 
-        if (height - worldScreenHeight > width - worldScreenWidth)
+		float scaleHeight = worldScreenHeight / height;
+		float scaleWidth = worldScreenWidth / width;
+
+		Vector3 scale = background.localScale;
+
+		if (scaleHeight < scaleWidth)
         {
-            Vector3 scale = background.localScale;
-            scale.x = worldScreenWidth / width;
-            scale.y = scale.x;
-            background.localScale = scale;
+			scale.x = scaleWidth;
+			scale.y = scaleWidth;
         }
         else
         {
-            Vector3 scale = background.localScale;
-            scale.x = worldScreenHeight / height;
-            scale.y = scale.x;
-            background.localScale = scale;
-        }
+			scale.x = scaleHeight;
+			scale.y = scaleHeight;
+		}
+
+		background.localScale = scale;
+
     }
 
 
-    public static List<Vector3> getFightScreenPostitions()
+    public static List<Vector3> getFightScreenPostitions(int friendCount, int enemyCount)
     {
+        float worldScreenHeight = Camera.main.orthographicSize * 2.0f;
+        float worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+
+        Vector3 leftDown = Camera.main.transform.position;
+        leftDown.x -= worldScreenWidth / 2.0f;
+        leftDown.y -= worldScreenHeight / 2.0f;
+
+        Vector3 rightDown = leftDown;
+        leftDown.x += worldScreenWidth;
+
+        float spaceBetweenCharactersAndScreen = 10;
+
+        float spacePartOfFriends = worldScreenHeight / (float)friendCount;
+        float spacePartOfEnemy = worldScreenHeight / (float)enemyCount;
+
+
 
         return null;
     }
