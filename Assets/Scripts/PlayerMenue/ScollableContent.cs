b983435c712b8e876ef4fc28e1d5a0eea6ae121e
Caterina Nahler <caterina.nahler@student.tugraz.at>
2015-07-02 09:42:58 +0200

    worked on player menue

---
+++ Assets/Scripts/PlayerMenue/ScollableContent.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using Assets.Scripts.Utils;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class ScollableContent : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IInitializePotentialDragHandler
+{
+    private ScrollRect srcollview;
+
+    public void Start()
+    {
+        srcollview = GetComponentInParent<ScrollRect>();
+    }
+
+    public void OnInitializePotentialDrag(PointerEventData eventData)
+    {
+        srcollview.OnInitializePotentialDrag(eventData);
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        srcollview.OnBeginDrag(eventData);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        srcollview.OnDrag(eventData);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        srcollview.OnEndDrag(eventData);
+    }
+}
